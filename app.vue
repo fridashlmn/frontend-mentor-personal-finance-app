0f421e3c264ccebc0ded02626a0e70d0651bdb1b
Frida <friederike.suhlmann@gmail.com>
2025-01-23 07:01:49 +0100

    feat: project init

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Using boostrap 5 with Nuxt</h1>
+    <button class="btn btn-primary">share</button>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,42 @@
+import svgLoader from 'vite-svg-loader'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+
+  devtools: { enabled: true },
+
+  app: {
+    baseURL: '',
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+        },
+      ],
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
+          tagPosition: 'bodyClose',
+        },
+      ],
+    },
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  vite: {
+    plugins: [svgLoader()],
+    optimizeDeps: {
+      include: ['fast-deep-equal'],
+    },
+  },
+})
